2ae4496add0478173250d9b617ceccab4831a8a3
haikal0423 <mhaikal23azka@gmail.com>
2024-05-30 15:15:44 +0700

    Menambahkan void remove

--- CircularQueuesArray_0242/CircularQueuesArray_0242.cpp
+++ CircularQueuesArray_0242/CircularQueuesArray_0242.cpp
@@ -2,46 +2,72 @@
 using namespace std;
 
 class Queues {
-private :
-	static const int max = 3;
-	int FRONT, REAR;
-	int queue_array[max];
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
 
-public :
-	Queues() {
-		FRONT = -1;
-		REAR = -1;
-	}
-
-	void insert() {
-		int num;
-		cout << "Enter a number: ";
-		cin >> num; 
-		cout << endl;
-
-	}
-
-    // Cek apakah antrian penuh
-    if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
-    {
-        cout << "\nQueue overflow\n";
-        return;
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
     }
 
-    // Cek apakah antrian kosong
-    if (FRONT == -1)
-    {
-        FRONT = 0;
-        REAR = 0;
-    }
+    void insert() {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
 
-    else
-    {
-        // Jika REAR berada di posisi terkahir array kembali keawal array
-        if (REAR == max - 1)
+        // Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
             REAR = 0;
+        }
+
         else
-            REAR = REAR + 1;
+        {
+            // Jika REAR berada di posisi terkahir array kembali keawal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+
     }
-    queue_array[REAR] = num;
+    void remove()
+    {
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe elemen deleted from the queue is : " << queue_array[FRONT] << endl;
+
+        // Cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+
+        else
+        {
+            // Jika elemen yang dihapus berada di posisi terakhir array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
 };
