92f1f57a2796dbe1633529d0b9372ad466c48c90
haikal0423 <mhaikal23azka@gmail.com>
2024-05-30 16:12:14 +0700

    Menambahkan default dengan cout invalid option

--- CircularQueuesArray_0242/CircularQueuesArray_0242.cpp
+++ CircularQueuesArray_0242/CircularQueuesArray_0242.cpp
@@ -142,3 +142,9 @@ int main() {
             case '4': {
                 return 0;
             }
+            default: {
+                cout << "Invalid option!!" << endl;
+                break;
+            }
+            }
+        }
