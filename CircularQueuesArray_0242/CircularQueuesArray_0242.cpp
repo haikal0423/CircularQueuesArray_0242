690295c76d3e23485ddfc0ed93574296d2655729
haikal0423 <mhaikal23azka@gmail.com>
2024-05-30 16:15:31 +0700

    Menambahkan catch dengan cout "Check for the values entered"

--- CircularQueuesArray_0242/CircularQueuesArray_0242.cpp
+++ CircularQueuesArray_0242/CircularQueuesArray_0242.cpp
@@ -148,3 +148,9 @@ int main() {
             }
             }
         }
+        catch (exception& e) {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+    return 0;
+}
